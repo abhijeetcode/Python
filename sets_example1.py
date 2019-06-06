1486a39d240a39b31453fc8c70605b2ae5d3d9ee
Abhijeet Adhikary (BSL) <abhijeeta2@kpit.com>
2019-06-06 18:06:12 +0530

    Set added

---
+++ sets_example1.py
@@ -0,0 +1,12 @@
+#A Set is an unordered collection data type that is iterable, mutable, and has no duplicate elements
+myset = set()
+myset.add(1)
+myset.add(2)
+myset.add(2)
+myset.add(3)
+print(myset)
+
+ll = {1,3,1,4,2,3,9,74,11,90,11,74}
+
+myset1 = set(ll)
+print(myset1)
